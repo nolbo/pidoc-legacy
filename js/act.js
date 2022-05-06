9e0cd64319b90d1846a6cc6c65e1d4bdb8df4286
nolbo <jun9975sun@gmail.com>
2022-05-06 23:30:52 +0900

    release 1

---
+++ js/act.js
@@ -0,0 +1,55 @@
+const con_padding = Number(getComputedStyle(document.documentElement).getPropertyValue('--content-padding-top').replace(/[^0-9.]/g, '')) * 16;
+
+matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    changeTheme('dark');
+});
+
+document.getElementById('theme').addEventListener('click', () => {
+    if(document.querySelector('link.media_dark').getAttribute('media') === '(prefers-color-scheme: dark)') {
+        changeTheme('light');
+    } else {
+        changeTheme('dark');
+    }
+});
+
+document.getElementById('sidebtn').addEventListener('click', () => {
+    if(document.querySelector('aside > ul').style.display === 'block') {
+        document.querySelectorAll('aside > ul').forEach(e => { e.style.display = 'none'; });
+    } else {
+        document.querySelectorAll('aside > ul').forEach(e => { e.style.display = 'block'; });
+    }
+});
+
+document.getElementById('sidebtn').addEventListener('blur', () => { document.querySelectorAll('aside > ul').forEach(e => { e.style.display = 'none'; }); });
+
+document.querySelectorAll('.list-el').forEach(e => {
+    e.addEventListener('click', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if(matchMedia('(hover: none) and (pointer: coarse)').matches) {
+            document.querySelectorAll('aside > ul').forEach(e => { e.style.display = 'none'; });
+        }
+        const pos = document.querySelector(`div.con#${e.id}`).getBoundingClientRect();
+        window.scrollTo(0, window.pageYOffset + pos.y - con_padding + ((e.id.split('-').length === 3) ? 4 : 0));
+    });
+});
+
+window.addEventListener('scroll', onScroll);
+
+function changeTheme(theme) {
+    document.querySelectorAll('link.media_dark').forEach(e => {
+        e.setAttribute('media', `(prefers-color-scheme: ${theme})`)
+    });
+}
+
+function onScroll() {
+    if(!matchMedia('(hover: none) and (pointer: coarse)').matches) {
+        if(document.querySelector('.list-el[data-selected]')) {
+            document.querySelector('.list-el[data-selected]').removeAttribute('data-selected');
+        }
+        let cons = [].slice.call(document.querySelectorAll('div.con')).filter(e => e.getBoundingClientRect().y <= con_padding);
+        document.querySelector(`.list-el#${cons[cons.length - 1].id}`).setAttribute('data-selected', '');
+    }
+}
+
+onScroll();
